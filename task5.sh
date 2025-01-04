39c86be9f8bd5992f1c61550a0342b1d32000d4b
raf-me <rafael.sitdikov07@gmail.com>
2025-01-04 14:25:26 +0300

    task5.sh

--- task5.sh
+++ task5.sh
@@ -4,3 +4,11 @@ rm volcarona4/zubatwoop
 rm volcarona4/zubatsandile
 rmdir wobbuffet7
 rm -r claydol6/chinchou
+
+chmod 755 volcarona4
+chmod 062 sandile7
+chmod 046 wooper8
+chmod 400 zubat
+chmod 312 claydol6
+chmod 511 wobbuffet7
+chmod 576 chinchou
