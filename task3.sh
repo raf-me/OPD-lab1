77e77bbcbc421b52bd7e31a0cde0b4c1c28d0e63
raf-me <rafael.sitdikov07@gmail.com>
2025-01-04 14:35:58 +0300

    task3.sh

--- task3.sh
+++ task3.sh
@@ -1,32 +1,10 @@
-chmod 755 bonsly4
 chmod 755 claydol6
-cd claydol6
-chmod 755 pikachu
-chmod 755 dodrio
-chmod 755 spoink
-chmod 755 chinchou
-cd ..
 chmod 755 sandile7
 chmod 755 volcarona4
 cd volcarona4
-chmod 755 lumineon
 chmod 755 zubat
-chmod 755 volcarona4
-cd volcarona4
-chmod 755 hariyama
-chmod 755 shieldon
-cd ..
-cd wobbuffet7
-chmod 755 munchlax
-chmod 755 dusclops
-chmod 755 munchlax
-cd ..
+cd..
 chmod 755 wooper8
-cd wobbuffet7
-chmod 755 conkeldurr
-chmod 755 dusclops
-chmod 755 munchlax
-cd ..
 chmod 755 wobbuffet7
 
 ln -s volcarona4 Copy50
@@ -35,5 +13,12 @@ cp wooper8 volcarona4/zubatwooper
 cat volcarona4/zubat volcarona4/zubat > wooper8_78
 cp -r volcarona4 wobbuffet7/munchlax
 cp wooper8 volcarona4
-ln -s wooper8 volcarona4/zubatwooper_1
+ln -s wooper8 volcarona4/zubatwooper
 ln sandile7 volcarona4/zubatsandile
+
+chmod 062 sandile7
+chmod 046 wooper8
+chmod 400 zubat
+chmod 312 claydol6
+chmod 511 wobbuffet7
+chmod 755 volcarona4
