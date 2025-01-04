20b9fb380fa42bd10cd46771f01cfcffec5b2eb5
raf-me <rafael.sitdikov07@gmail.com>
2025-01-04 16:46:45 +0300

    task4.sh

--- task4.sh
+++ task4.sh
@@ -1,9 +1,6 @@
 cat zubat 2>&1| wc -c > /tmp/zubat_char_count 2>&1
-cd ..
-grep -r "wo" lab0
-cd lab0
 ls -lruR | grep -E "[0-9]{2}:[0-9]{2} *wo*" | sort -k2,2nr
-ls -1R | grep '^s' | sort -r
-grep -ni "Ro" volcarona4/zubat
+ls -1R 2>&1 | grep '^s' | sort -r
+grep -ni "Ro" volcarona4/zubat 2>&1
 ls volcarona4 2>&1 | sort -r
 ls claydol6 | sort
