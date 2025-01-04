959d98abed4091422b1d4b437c38577611c327b8
raf-me <rafael.sitdikov07@gmail.com>
2025-01-04 16:36:53 +0300

    task4.sh

--- task4.sh
+++ task4.sh
@@ -3,7 +3,7 @@ cd ..
 grep -r "wo" lab0
 cd lab0
 ls -lruR | grep -E "[0-9]{2}:[0-9]{2} *wo*" | sort -k2,2nr
-ls -1R | grep '^s' | cat | sort -r
+ls -1R | grep '^s' | sort -r
 grep -ni "Ro" volcarona4/zubat
 ls volcarona4 2>&1 | sort -r
 ls claydol6 | sort
