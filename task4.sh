59fa071242e87a422ae56689d7c548245d573024
raf-me <rafael.sitdikov07@gmail.com>
2025-01-11 16:25:07 +0300

    task4.sh

--- task4.sh
+++ task4.sh
@@ -1,7 +1,7 @@
 cat zubat 2>&1| wc -c > /tmp/zubat_char_count 2>&1
 #ls -lruR | grep -E "[0-9]{2}:[0-9]{2} *wo*" | sort -k2,2nr - использование через регулярные выражения
 ls -Rrl | grep "wo" | sort -rk2
-ls -1R 2>&1 | grep '^s' | sort -r
+ls -1R | grep '^s' | xargs cat | sort -rk6
 grep -ni "Ro" volcarona4/zubat 2>&1
 ls volcarona4 2>&1 | sort -r
 ls claydol6 | sort
